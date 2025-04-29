2395247ce88f46deb8291448e79ef9442a69e279
Lei Zhi Guang <rayguang123@gmail.com>
2025-04-29 15:05:44 +0800

    add config to github

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    SUPABASE_URL: 'https://cnxkfuwuoahnhxbcnnol.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNueGtmdXd1b2Fobmh4YmNubm9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMDc4NTQsImV4cCI6MjA2MDg4Mzg1NH0.qkEF0_OifXj6-Y4b5kwWgEmcw0IAF2YtbWs_F1_9u_k',
+    STORAGE_BUCKET: 'ads-media',
+    DATABASE_TABLE: 'facebook_ads'
+};
+
+// Make config available globally
+window.config = config; 
